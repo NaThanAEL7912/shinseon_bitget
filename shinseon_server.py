7e965078d6aba3e9da1991e1f23488be45188e2c
King <king@shinseon.com>
2026-08-29 13:29:23 +0900

    Deploy version V7.68

--- shinseon_server.py
+++ shinseon_server.py
@@ -860,7 +860,7 @@ def append_daily_csv_record(row_str):
 # ---- BOT CORE AND ENGINE ----
 class BotCore:
     def __init__(self):
-        self.CURRENT_VERSION = "V7.67"
+        self.CURRENT_VERSION = "V7.68"
         from collections import deque
         self.c_total = 20000.0
         self.m_bitget = 20000.0
